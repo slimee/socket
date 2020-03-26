0fd6016b63f4a409e8c6ba16e20dde1574877aae
slim <smedini@gmail.com>
2020-03-26 08:41:20 +0100

    indent

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ var server = require('http').createServer(app)
 var io = require('socket.io')(server)
 var port = process.env.PORT || 3000
 
-server.listen(port, function(){
+server.listen(port, function () {
   console.log('Server listening at port %d', port)
 })
-
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')))
@@ -30,55 +29,52 @@ io.on('connection', (socket) => {
     })
   })
 
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    console.log('welcome', username)
+    if (addedUser) return
 
-// when the client emits 'add user', this listens and executes
-socket.on('add user', (username) => {
-  if(addedUser) return
-
-// we store the username in the socket session for this client
+    // we store the username in the socket session for this client
     socket.username = username
-++numUsers
-addedUser = true
-socket.emit('login', {
-  numUsers: numUsers,
-})
-// echo globally (all clients) that a person has connected
-socket.broadcast.emit('user joined', {
-  username: socket.username,
-  numUsers: numUsers,
-})
-})
-
-
-// when the client emits 'typing', we broadcast it to others
-socket.on('typing', () => {
-  socket.broadcast.emit('typing', {
-    username: socket.username,
-  })
-})
-
-
-// when the client emits 'stop typing', we broadcast it to others
-socket.on('stop typing', () => {
-  socket.broadcast.emit('stop typing', {
-    username: socket.username,
-  })
-})
-
-
-// when the user disconnects.. perform this
-socket.on('disconnect', () => {
-  if(addedUser) {
-    --numUsers
-
-    // echo globally that this client has left
-    socket.broadcast.emit('user left', {
+    ++numUsers
+    addedUser = true
+    socket.emit('login', {
+      numUsers: numUsers,
+    })
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
       username: socket.username,
       numUsers: numUsers,
     })
-  }
-}
-)
+  })
+
+// when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username,
+    })
+  })
+
+// when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username,
+    })
+  })
+
+// when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+      if (addedUser) {
+        --numUsers
+
+        // echo globally that this client has left
+        socket.broadcast.emit('user left', {
+          username: socket.username,
+          numUsers: numUsers,
+        })
+      }
+    },
+  )
 
 })
 
