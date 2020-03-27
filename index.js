3ba9abba22c283a4999e1c69e5b3aba58933f7ad
slim <smedini@gmail.com>
2020-03-27 22:50:24 +0100

    login, message

--- index.js
+++ index.js
@@ -1,80 +1,47 @@
-// Setup basic express server
-var express = require('express')
-var app = express()
-var path = require('path')
-var server = require('http').createServer(app)
-var io = require('socket.io')(server)
-var port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const path = require('path')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const port = process.env.PORT || 3000
 
-server.listen(port, function () {
-  console.log('Server listening at port %d', port)
-})
-
-// Routing
+server.listen(port, () => console.log('Server listening at port %d', port))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Chatroom
-
-var numUsers = 0
-
+let players = []
+let messageCount = 0
 io.on('connection', (socket) => {
-  var addedUser = false
-
-// when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data,
-    })
+  let addedUser = false
+  socket.on('new message', (content) => {
+    const message = {
+      id: ++messageCount,
+      player: socket.player,
+      content,
+    }
+    socket.emit('new message', message)
+    socket.broadcast.emit('new message', message)
   })
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('login', (username) => {
-    console.log('welcome', username)
+  socket.on('login', (player) => {
     if (addedUser) return
-
-    // we store the username in the socket session for this client
-    socket.username = username
-    ++numUsers
+    console.log('login', player)
+    socket.player = player
+    players.push(socket.player)
     addedUser = true
-    socket.emit('login', {
-      numUsers: numUsers,
-    })
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers,
-    })
+    socket.emit('login', { player, players: players.filter(p => p !== player) })
+    socket.broadcast.emit('user joined', player)
   })
-
-// when the client emits 'typing', we broadcast it to others
   socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username,
-    })
+    socket.broadcast.emit('typing', { username: socket.username })
   })
-
-// when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username,
-    })
+    socket.broadcast.emit('stop typing', { username: socket.username })
   })
-
-// when the user disconnects.. perform this
   socket.on('disconnect', () => {
       if (addedUser) {
-        --numUsers
-
-        // echo globally that this client has left
-        socket.broadcast.emit('user left', {
-          username: socket.username,
-          numUsers: numUsers,
-        })
+        players = players.filter(player => player.id !== socket.player.id)
+        socket.broadcast.emit('user left', socket.player)
       }
     },
   )
-
 })
 
